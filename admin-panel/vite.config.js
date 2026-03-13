5f0ff09aa2b2873260697c3b7e0df82fa17d6765
Ronit Sarkar <rseditz57@gmail.com>
2026-03-13 14:32:31 +0530

    force push files

--- admin-panel/vite.config.js
+++ admin-panel/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './', // Use relative paths for assets
     plugins: [react(), tailwindcss()],
     server: {
         port: 3001,
